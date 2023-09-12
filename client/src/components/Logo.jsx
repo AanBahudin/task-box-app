19d0701abae307efa015938000de831ccea8310f
Aan Bahudin <aan.bahudin11@gmail.com>
2023-09-12 22:35:43 +0800

    reset & setup dashboard page

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/prop-types */
-import logo from '../assets/images/logo1.png'
 
 const Logo = ({center}) => {
   return (
-    <img className={`${center ? 'mx-auto' : null} w-14 h-14 cursor-default`} src={logo} alt="" />
+    <h1>logo</h1>
   )
 }
 
--- client/src/pages/Todo.jsx
+++ client/src/pages/Todo.jsx
@@ -16,35 +16,16 @@ export const action = async({ request }) => {
     }
 }
 
-
-const styleSection = `
-    w-full md:w-[80%] h-[100vh]  mx-auto flex items-center justify-center flex-col
-`
-
 const Todo = () => {
 
     const {jobData} = useOutletContext()
     const {todo} = jobData
 
     return (
-        <div className="w-[100vw] min-h-[100vh] bg-[#030C0E]">
-            
-            <section className={styleSection}>
-
-                {/* <h1 className="text-[#BDBEBC] text-4xl font-medium">Todo List</h1> */}
-
-                <Form method="POST" className="mt-8 w-[90%] md:w-[60%] py-2 px-7  rounded-2xl mx-auto text-[#BDBEBC] bg-[#1C2E34] flex justify-between items-center">
-                    <input className="w-[90%] bg-transparent outline-none border-none py-2" type="text" name="todo" id="todo" placeholder="add your text" />
-                    <p>add</p>
-                </Form>
-
-                <div className="w-[90%] md:w-[60%] my-8">
-                    {todo.length !== 0 ? <TodoContainer todo={todo}  /> : <h1 className="text-center mt-10 text-goldenWhite text-3xl">no task recently ...</h1>}
-                </div>
-
-            </section>
-        </div>
-        )
+        <div>
+            Todo page
+        </div>    
+    )
 }
 
 export default Todo
