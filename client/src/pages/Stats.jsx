441f4e04a2f9fc2ee8e349c5e3517e866a9625ae
Aan Bahudin <aan.bahudin11@gmail.com>
2023-09-13 21:37:24 +0800

    working on stats (priority task)

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,7 +1,7 @@
 
 const Stats = () => {
   return (
-    <div className="p-20 w-full max-h-[100vh] scroll-smooth overflow-y-auto text-bluePrimary duration-200 ease-in-out">
+    <div className="p-20 w-full max-h-[100vh] scroll-smooth overflow-y-auto text-bluePrimary duration-200 ease-in-out cursor-default">
       <section>
         <h1 className="text-2xl font-semibold text-purplePrimary">Tasks Management Overview</h1>
         <p className="mt-5 text-sm">Tasks Management Overview provides a comprehensive view of your task-related activities, helping you efficiently organize, track, and prioritize your tasks. Gain insights into your task management process and stay in control of your workload.</p>
@@ -9,7 +9,7 @@ const Stats = () => {
         {/* CARD SECTION */}
         <div className="grid grid-cols-4 gap-x-10 text-bluePrimary mt-10">
 
-          <div className="bg-greyPrimary rounded-md py-1 px-1 h-mediumCard w-mediumCard min-h-[120px] max-h-[120px] border-[0.5px] border-black drop-shadow-xl">
+          <div className="bg-greyPrimary rounded-md py-1 px-1 h-mediumCard w-mediumCard min-h-[120px] max-h-[120px] border-[0.5px] border-black drop-shadow-xl cursor-default hover">
             <h4 className="text-xs mt-1">TOTAL TASK CREATED</h4>
             <p className="text-center text-5xl mt-5 font-semibold text-bluePrimary h-full">20</p>
           </div>
@@ -74,23 +74,58 @@ const Stats = () => {
         <h1 className="text-2xl font-semibold text-purplePrimary">Priority Tasks Overview</h1>
         <p className="mt-5 text-sm">The Priority Task Overview provides a concise summary of your most important and time-sensitive tasks. It offers a quick glance at high-priority items, allowing you to focus on what needs immediate attention and ensuring you stay on top of critical assignments</p>
 
-        <div className="grid grid-cols-4 justify-items-center w-full gap-x-10 text-bluePrimary mt-10">
+        <div className="flex justify-between w-[80%] mx-auto gap-x-10 text-bluePrimary mt-10">
 
           <div className="bg-success rounded-md py-1 px-1 h-[120px] w-mediumCard border-[0.5px] border-black drop-shadow-xl">
-            <h4 className="text-xs mt-1">COMPLETED TASKS</h4>
+            <h4 className="text-xs mt-1">LESS PRIORITY TASKS</h4>
             <p className="text-center text-5xl mt-5 font-semibold text-bluePrimary h-full">20</p>
           </div>
 
           <div className="bg-warning rounded-md py-1 px-1 h-[120px] w-mediumCard border-[0.5px] border-black drop-shadow-xl">
-            <h4 className="text-xs mt-1">TASKS IN PROGRESS</h4>
+            <h4 className="text-xs mt-1">MEDIUM PRIORITY TASKS</h4>
             <p className="text-center text-5xl mt-5 font-semibold text-bluePrimary h-full">20</p>
           </div>
 
           <div className="bg-danger rounded-md py-1 px-1 h-[120px] w-mediumCard border-[0.5px] border-black drop-shadow-xl">
-            <h4 className="text-xs mt-1">TASKS ON HOLD</h4>
+            <h4 className="text-xs mt-1">HIGH PRIORITY TASKS</h4>
             <p className="text-center text-5xl mt-5 font-semibold text-bluePrimary h-full">20</p>
           </div>
           
+        </div>
+
+        {/* PRIORITY TASKS TABLE */}
+        <div className="mt-10 w-full bg-greyPrimary p-4 rounded-lg min-h-[300px]">
+          <h3 className="font-semibold text-xl text-purplePrimary mt-4">Priority Tasks</h3>
+          <p className="text-sm mt-2">View and manage high-priority tasks that require immediate attention.  </p>
+
+          <article className="mx-auto mt-10 flex flex-col gap-y-2">
+
+              <div className="flex items-stretch justify-between py-2 bg-white text-sm px-4 border rounded-md">
+                <p className="truncate my-auto">1</p>
+                <h5 className="truncate my-auto">Meeting with Marketing Team</h5>
+                <h5 className="truncate my-auto">Monday, 1st</h5>
+                <h5 className="bg-danger text-white py-1 px-2 rounded-md">On Hold</h5>
+                <h5 className="bg-danger text-white py-1 px-2 rounded-md">High </h5>
+              </div>
+
+              <div className="flex items-stretch justify-between bg-white  py-2 text-sm px-4 rounded-md">
+                <p className="truncate my-auto">1</p>
+                <h5 className="truncate my-auto">Meeting with Marketing Team</h5>
+                <h5 className="truncate my-auto">Monday, 1st</h5>
+                <h5 className="bg-danger text-white py-1 px-2 rounded-md">On Hold</h5>
+                <h5 className="bg-warning text-white py-1 px-2 rounded-md">Mid</h5>
+              </div>
+
+              <div className="flex items-stretch justify-between bg-white py-2 text-sm px-4 rounded-md">
+                <p className="truncate my-auto">1</p>
+                <h5 className="truncate my-auto">Meeting with Marketing Team</h5>
+                <h5 className="truncate my-auto">Monday, 1st</h5>
+                <h5 className="bg-danger text-white py-1 px-2 rounded-md">On Hold</h5>
+                <h5 className="bg-danger text-white py-1 px-2 rounded-md">High </h5>
+              </div>
+          </article>
+
+          <p className="text-right mt-10 text-sm hover:underline hover:font-semibold text-purplePrimary cursor-default duration-200 ease-in-out">Show All ...</p>
         </div>
       </section>
     </div>
