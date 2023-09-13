9f761d17299550ef6c6f864a238ac7955d57c326
Aan Bahudin <aan.bahudin11@gmail.com>
2023-09-13 22:00:25 +0800

    working on stats (adding icons)

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,31 +1,33 @@
+import { BsClipboard2Fill, BsClipboard2CheckFill, BsClipboard2PulseFill, BsClipboard2XFill, BsExclamationTriangleFill, BsFillAlarmFill, BsFillInfoSquareFill } from 'react-icons/bs'
+
 
 const Stats = () => {
   return (
     <div className="p-20 w-full max-h-[100vh] scroll-smooth overflow-y-auto text-bluePrimary duration-200 ease-in-out cursor-default">
       <section>
-        <h1 className="text-2xl font-semibold text-purplePrimary">Tasks Management Overview</h1>
+        <h1 className="text-2xl font-semibold text-purplePrimary flex items-center gap-x-4"> <BsFillInfoSquareFill />Tasks Management Overview</h1>
         <p className="mt-5 text-sm">Tasks Management Overview provides a comprehensive view of your task-related activities, helping you efficiently organize, track, and prioritize your tasks. Gain insights into your task management process and stay in control of your workload.</p>
 
         {/* CARD SECTION */}
         <div className="grid grid-cols-4 gap-x-10 text-bluePrimary mt-10">
 
           <div className="bg-greyPrimary rounded-md py-1 px-1 h-mediumCard w-mediumCard min-h-[120px] max-h-[120px] border-[0.5px] border-black drop-shadow-xl cursor-default hover">
-            <h4 className="text-xs mt-1">TOTAL TASK CREATED</h4>
+            <h4 className="text-xs mt-1 flex gap-x-2 ml-3"><span className='my-auto'><BsClipboard2Fill /></span>  TOTAL TASK CREATED</h4>
             <p className="text-center text-5xl mt-5 font-semibold text-bluePrimary h-full">20</p>
           </div>
 
           <div className="bg-success rounded-md py-1 px-1 h-mediumCard w-mediumCard border-[0.5px] border-black drop-shadow-xl">
-            <h4 className="text-xs mt-1">COMPLETED TASKS</h4>
+            <h4 className="text-xs mt-1 flex gap-x-2 ml-3"><span className='my-auto'><BsClipboard2CheckFill /></span>  COMPLETED TASKS</h4>
             <p className="text-center text-5xl mt-5 font-semibold text-bluePrimary h-full">20</p>
           </div>
 
           <div className="bg-warning rounded-md py-1 px-1 h-mediumCard w-mediumCard border-[0.5px] border-black drop-shadow-xl">
-            <h4 className="text-xs mt-1">TASKS IN PROGRESS</h4>
+            <h4 className="text-xs mt-1 flex gap-x-2 ml-3"><span className='my-auto'><BsClipboard2PulseFill /></span>  TASKS IN PROGRESS</h4>
             <p className="text-center text-5xl mt-5 font-semibold text-bluePrimary h-full">20</p>
           </div>
 
           <div className="bg-danger rounded-md py-1 px-1 h-mediumCard w-mediumCard border-[0.5px] border-black drop-shadow-xl">
-            <h4 className="text-xs mt-1">TASKS ON HOLD</h4>
+            <h4 className="text-xs mt-1 flex gap-x-2 ml-3"><span className='my-auto'><BsClipboard2XFill /></span>  TASKS ON HOLD</h4>
             <p className="text-center text-5xl mt-5 font-semibold text-bluePrimary h-full">20</p>
           </div>
         </div>
@@ -34,7 +36,7 @@ const Stats = () => {
       </section>
 
       <section>
-        <h1 className="text-2xl font-semibold text-purplePrimary">Recently Added Tasks</h1>
+        <h1 className="text-2xl font-semibold text-purplePrimary flex items-center gap-x-4"> <BsFillAlarmFill />Recently Added Tasks</h1>
         <p className="mt-5 text-sm">Recently Added Tasks to view the latest additions to your to-do list. Stay up-to-date with your most recent commitments and ensure nothing slips through the cracks.</p>
 
         {/* CARDS */}
@@ -71,7 +73,7 @@ const Stats = () => {
 
       <section>
 
-        <h1 className="text-2xl font-semibold text-purplePrimary">Priority Tasks Overview</h1>
+        <h1 className="text-2xl font-semibold text-purplePrimary flex items-center gap-x-4"> <BsExclamationTriangleFill /> Priority Tasks Overview</h1>
         <p className="mt-5 text-sm">The Priority Task Overview provides a concise summary of your most important and time-sensitive tasks. It offers a quick glance at high-priority items, allowing you to focus on what needs immediate attention and ensuring you stay on top of critical assignments</p>
 
         <div className="flex justify-between w-[80%] mx-auto gap-x-10 text-bluePrimary mt-10">
