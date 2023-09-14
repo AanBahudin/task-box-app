32e3e29f58031c25f2bddca85a67ee53aaead06c
Aan Bahudin <aan.bahudin11@gmail.com>
2023-09-14 12:13:45 +0800

    send task from client to database

--- client/src/pages/AddTask.jsx
+++ client/src/pages/AddTask.jsx
@@ -1,21 +1,29 @@
 /* eslint-disable react-refresh/only-export-components */
 
-import { Form, redirect } from "react-router-dom"
+import { Form, redirect, useNavigation } from "react-router-dom"
 import { FormInput, TextInput, FormSelect } from "../components"
 import { taskCategory, taskPriority } from "../utils/helper"
+import { toast } from "react-toastify"
+import axios from "axios"
 
 export const addTaskAction = async({request}) => {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+
   try {
-    const formData = await request.formData()
-    const data = Object.fromEntries(formData)
-    console.log(data);
+    await axios.post('/api/v1/todo', data)
+    toast.success('Task Created !')
     return redirect('.')
   } catch (error) {
+    toast.error(error.response.data.msg)
     return redirect('.')
   }
 }
 
 const AddTask = () => {
+
+  const isSubmitting = useNavigation().state === 'submitting'
+
   return (
     <div className="w-full max-h-[100vh] overflow-y-auto p-20 text-bluePrimary">
        <h1 className="text-2xl font-semibold text-purplePrimary flex items-center gap-x-4"> Create a New Task</h1>
@@ -31,7 +39,7 @@ const AddTask = () => {
           </div>
 
           <div className="w-1/2 ml-auto grid grid-cols-2 gap-x-3 mt-5">
-            <button type='submit' className='cursor-default w-full disabled:cursor-not-allowed hover:bg-[#854c9c] bg-purplePrimary text-white text-center py-2 px-6 rounded-md my-5'>Save</button>
+            <button type='submit' className='cursor-default w-full disabled:cursor-not-allowed hover:bg-[#854c9c] bg-purplePrimary text-white text-center py-2 px-6 rounded-md my-5'>{isSubmitting ? 'Saving ...' : 'Save'}</button>
             <button type='reset' className='cursor-default w-full disabled:cursor-not-allowed hover:bg-[#EA6060] bg-danger text-white text-center py-2 px-6 rounded-md my-5'>Cancel</button>
           </div>
 
