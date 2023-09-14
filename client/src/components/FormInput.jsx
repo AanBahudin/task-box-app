7ba0b626d88da71334b17bb2a7995c26a7d43000
Aan Bahudin <aan.bahudin11@gmail.com>
2023-09-14 12:14:36 +0800

    fix small bug

--- client/src/components/FormInput.jsx
+++ client/src/components/FormInput.jsx
@@ -4,7 +4,7 @@ const FormInput = ({ labelText, labelFor, type, defaultValue, name, isFocus }) =
   return (
     <div className={`flex max-w-full flex-col justify-start items-start`}>
         <label htmlFor={labelFor}>{labelText}</label>
-        <input defaultValue={defaultValue} autoFocus={isFocus} className="px-5 py-1 mt-2 w-full rounded-lg bg-white border-[1px] border-bluePrimary" type={type} name={name} id={name} />
+        <input defaultValue={defaultValue} autoFocus={isFocus} className="px-5 py-2 text-sm mt-2 w-full rounded-lg bg-white border-[1px] border-bluePrimary" type={type} name={name} id={name} />
     </div>
   )
 }
