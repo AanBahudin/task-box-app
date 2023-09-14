d17b314520847630ef625e729c1e1af1b8cb9c11
Aan Bahudin <aan.bahudin11@gmail.com>
2023-09-14 23:04:07 +0800

    profile card

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -33,8 +33,34 @@ const Profile = () => {
   // const isSubmitting = navigation.state === 'submitting'
   
   return (
-    <div>
-      profile
+    <div className='p-20 w-full max-h-[100vh] flex justify-center items-center gap-x-10'>
+
+      <section className='min-h-[60vh] bg-greyPrimary drop-shadow-xl rounded-md p-5 w-[25%]'>
+        <div className='w-20 h-20 bg-white rounded-full mx-auto'></div>
+        <h5 className='text-center text-sm font-semibold text-purplePrimary mt-3'>Aan Bahudin</h5>
+        <p className='text-center text-xs mt-1 text-bluePrimary'>aanbahudin@gmail.com</p>
+
+        <article className='mt-16'>
+          <div className='w-[90%] mx-auto flex justify-between text-xs text-bluePrimary mt-5'>
+            <h5 className='text-purplePrimary'>Joined Since</h5>
+            <p>May, 1st</p>
+          </div>
+
+          <div className='w-[90%] mx-auto flex justify-between text-xs text-bluePrimary mt-5'>
+            <h5 className='text-purplePrimary'>Last Updated</h5>
+            <p>June, 5th</p>
+          </div>
+
+          <div className='w-[90%] mx-auto flex justify-between text-xs text-bluePrimary mt-5'>
+            <h5 className='text-purplePrimary'>Total Tasks</h5>
+            <p>43</p>
+          </div>
+        </article>
+      </section>
+
+      <section className='min-h-[60vh] bg-greyPrimary drop-shadow-xl rounded-md p-5 w-[70%]'>
+
+      </section>
     </div>
   )
 }
