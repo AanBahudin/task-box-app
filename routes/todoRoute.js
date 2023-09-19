649ed5af11ed8970fab142527dabc6060ed9152f
Aan Bahudin <aan.bahudin11@gmail.com>
2023-09-19 13:05:46 +0800

    remove aggregate test route

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -1,14 +1,12 @@
 import express from 'express'
 const router = express.Router()
 
-import { getAllTodos, getSingleTodo, updateTodo, deleteTodo, createTodo, aggreageTest } from '../controllers/todoControllers.js'
+import { getAllTodos, getSingleTodo, updateTodo, deleteTodo, createTodo } from '../controllers/todoControllers.js'
 import { validateTodoInput, validateParamTodo } from '../middleware/inputValidationMiddleware.js'
 
 router.route('/')
     .get(getAllTodos)
     .post(validateTodoInput,createTodo)
-
-router.route('/test-aggregate').get(aggreageTest)
 
 router.route('/:id')
     .get(validateParamTodo, getSingleTodo)
