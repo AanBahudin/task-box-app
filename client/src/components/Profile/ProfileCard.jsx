6e85a4f5a4b4ea0ed9e24997d5b64c71e11c8a80
Aan Bahudin <aan.bahudin11@gmail.com>
2023-09-23 22:25:21 +0800

    stats fixing (profile card image default)

--- client/src/components/Profile/ProfileCard.jsx
+++ client/src/components/Profile/ProfileCard.jsx
@@ -2,12 +2,18 @@
 // eslint-disable-next-line no-unused-vars
 
 import moment from 'moment'
+import { BiSolidUser } from 'react-icons/bi'
 
 const ProfileCard = ({user}) => {
 
   return (
      <section className='min-h-[60vh] bg-greyPrimary drop-shadow-xl rounded-md p-5 w-[25%]'>
-        <img className='w-20 h-20 bg-white rounded-full mx-auto' src={user.avatar} alt="avatar" />
+
+        {user.avatar ? (
+          <img className='w-20 h-20 bg-white rounded-full mx-auto' src={user.avatar} alt="avatar" />
+        ) : (
+          <div className='w-20 h-20 bg-white rounded-full mx-auto flex items-center justify-center'> <BiSolidUser className='fill-bluePrimary w-16 h-16' /> </div>
+        )}
 
         <h5 className='text-center text-sm font-semibold text-purplePrimary mt-3'>{user.name}</h5>
         <p className='text-center text-xs mt-1 text-bluePrimary'>{user.email}</p>
