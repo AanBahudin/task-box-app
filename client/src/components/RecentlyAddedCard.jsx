bfee6ba667b0d39c27ab40022f2855fd3c039d2e
Aan Bahudin <aan.bahudin11@gmail.com>
2023-09-23 22:02:11 +0800

    stats fixing (complete basic sync in stats page)

--- client/src/components/RecentlyAddedCard.jsx
+++ client/src/components/RecentlyAddedCard.jsx
@@ -1,14 +1,19 @@
-import { recentlyAddedTasksExample } from '../utils/helper'
+import { useDashboardContext } from '../pages/Dashboard'
 
 const RecentlyAddedCard = () => {
+
+  const {todoData} = useDashboardContext()
+  const newTodo = todoData.todos.slice(0,4)
+
+
   return (
     <div className="grid grid-cols-4 gap-x-7 mt-10">
 
-        {recentlyAddedTasksExample.map((item, index) => {
+        {newTodo.map((item, index) => {
             return (
                 <div key={index} className="bg-greyPrimary duration-200 p-4 rounded-md border-[0.5px] border-black hover:drop-shadow-xl cursor-default">
-                    <h5 className="truncate text-sm font-semibold">{item.title}</h5>
-                    <p className="text-xs my-3">{item.text}</p>
+                    <h5 className="truncate text-sm font-semibold ">{item.task}</h5>
+                    <p className="text-xs my-3 truncate">{item.description}</p>
                     <p className="text-center text-purplePrimary text-xs hover:underline hover:font-semibold cursor-default">Show Detail</p>
                 </div>
             )
